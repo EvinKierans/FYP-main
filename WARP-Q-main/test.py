2d824f69de445f7be25ffc9f9fe3c5cef3733cac
Evin Kierans <44497184+EvinKierans@users.noreply.github.com>
2022-04-29 15:44:57 +0100

    Delete test.py

--- WARP-Q-main/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-training_data_df = pd.read_csv("Results.csv", header=0)
-training_data_df = training_data_df.T
-print(training_data_df[0])
-training_data_df.to_csv("Results_check.csv")
